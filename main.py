c96d4686167c366310f7b04a736b04171a9bcd5b
Madhur Rastogi <madhurrastogi20@gmail.com>
2026-02-21 17:01:52 +0530

    Clean up main.py for better performance
    Removed comments and unnecessary sleep to improve performance.

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import serial
 import pyautogui
 import time
 
-# Update the baud rate to match the Arduino (115200)
+
 arduino = serial.Serial('COM9', 115200, timeout=0.01)
 
-# PyAutoGUI has a default 0.1s delay after every action. 
-# SET THIS TO ZERO to remove the built-in lag!
 pyautogui.PAUSE = 0
 
 print("System Active. Move the joystick")
@@ -16,7 +14,7 @@ while True:
         try:
             command = arduino.readline().decode("utf-8").strip()
             
-            # Use 'press' but keep the logic fast
+            
             if command == "LEFT":
                 pyautogui.press('left')
             elif command == "RIGHT":
@@ -26,7 +24,5 @@ while True:
             elif command == "DOWN":
                 pyautogui.press('down')
         except:
-            continue # Ignore corrupted serial data
+            continue 
             
-    # Keep this very small or remove it for maximum speed
-    # time.sleep(0.001)
